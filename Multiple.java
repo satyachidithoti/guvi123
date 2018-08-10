696ab00be8de9ab519d7730ec9613ebf8a679732
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-10 14:05:34 +0100

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Multiples
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<5;i++)
+		System.out.print(n*i+" ");
+ 
+		System.out.print(n*5);
+ 
+	}
+}
