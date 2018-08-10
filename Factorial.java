ec82fb2faf526927565a4bfd4f76543c8b30cd50
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-10 14:01:57 +0100

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class Factorial
+{
+   public static void main(String[] args)
+ 	{
+       int n = 7;
+       int result = 1;
+       for (int i = 1; i <= n; i++) 
+	{
+           result = result * i;
+       }
+       System.out.println("The factorial of 7 is " + result);
+   }
+}
